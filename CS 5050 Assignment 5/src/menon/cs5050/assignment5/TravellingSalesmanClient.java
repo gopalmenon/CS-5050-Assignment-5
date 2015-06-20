0af00f538e8f4b7261b43058854892b9f0648690
gopalmenon <gopal.mnn@gmail.com>
2015-06-19 23:24:08 -0600

    Updated statistics collection to do multiple runs and compute averages.

--- CS 5050 Assignment 5/src/menon/cs5050/assignment5/TravellingSalesmanClient.java
+++ CS 5050 Assignment 5/src/menon/cs5050/assignment5/TravellingSalesmanClient.java
@@ -9,8 +9,9 @@ import java.util.Random;
 
 public class TravellingSalesmanClient {
 	
-	public static final int MINIMUM_NUMBER_OF_CITIES = 2;
-	public static final int MAXIMUM_NUMBER_OF_CITIES = 13;
+	public static final int MINIMUM_NUMBER_OF_CITIES = 3;
+	public static final int MAXIMUM_NUMBER_OF_CITIES = 12;
+	public static final int NUMBER_OF_RUNS_TO_AVERAGE = 5;
 	public static final String OUTPUT_FILE_NAME = "output.txt";
 	public static final String SCRIPT_GLOBAL_CODE = "\nlog.a.exhaustive = log2(a.exhaustive)" +
 													"\nlog.b.global.upper = log2(b.global.upper)" +
@@ -42,7 +43,7 @@ public class TravellingSalesmanClient {
 	 */
 	public TravellingSalesmanClient() {
 		
-		this.randomDoubleGenerator = new Random(2038905);
+		this.randomDoubleGenerator = new Random();
 		
 		int numberOfTests = MAXIMUM_NUMBER_OF_CITIES - MINIMUM_NUMBER_OF_CITIES + 1;
 		this.problemSize = new ArrayList<Integer>(numberOfTests);
@@ -96,7 +97,7 @@ public class TravellingSalesmanClient {
 	}
 
 
-	private void runAllTours(int numberOfCities) {
+	private void runAllTours(int numberOfCities, int runIndex, int expandedStatesListIndex) {
 		
 		City startCity = getNextRandomCity();
 		List<City> citiesToTour = getCitiesToTour(numberOfCities);
@@ -104,31 +105,57 @@ public class TravellingSalesmanClient {
 		try {
 			
 			//Add 1 to take into account the starting city
-			this.problemSize.add(Integer.valueOf(numberOfCities + 1));
-			
+			if (runIndex == 0) {
+				this.problemSize.add(Integer.valueOf(numberOfCities + 1));
+			}
 			TravellingSalesman travellingSalesmanA = new TravellingSalesman(startCity, citiesToTour, TravellingSalesman.EXHAUSTIVE_SOLUTION);
 			tour = travellingSalesmanA.getShortestTour();
-			this.evaluatedStatesAlorithmA.add(Integer.valueOf(tour.getNumberOfStatesExpanded()));
+			if (runIndex != 0) {
+				this.evaluatedStatesAlorithmA.set(expandedStatesListIndex, Integer.valueOf(this.evaluatedStatesAlorithmA.get(expandedStatesListIndex).intValue() + tour.getNumberOfStatesExpanded()));  
+			} else {
+				this.evaluatedStatesAlorithmA.add(Integer.valueOf(tour.getNumberOfStatesExpanded()));
+			}
+			
 			
 			TravellingSalesman travellingSalesmanB = new TravellingSalesman(startCity, citiesToTour, TravellingSalesman.GLOBAL_UPPER_BOUND_SOLUTION);
 			tour = travellingSalesmanB.getShortestTour();
-			this.evaluatedStatesAlorithmB.add(Integer.valueOf(tour.getNumberOfStatesExpanded()));
+			if (runIndex != 0) {
+				this.evaluatedStatesAlorithmB.set(expandedStatesListIndex, Integer.valueOf(this.evaluatedStatesAlorithmB.get(expandedStatesListIndex).intValue() + tour.getNumberOfStatesExpanded()));  
+			} else {
+				this.evaluatedStatesAlorithmB.add(Integer.valueOf(tour.getNumberOfStatesExpanded()));
+			}
 
 			TravellingSalesman travellingSalesmanC = new TravellingSalesman(startCity, citiesToTour, TravellingSalesman.LOCAL_LOWER_BOUND_SOLUTION_FROM_CLASS);
 			tour = travellingSalesmanC.getShortestTour();
-			this.evaluatedStatesAlorithmC.add(Integer.valueOf(tour.getNumberOfStatesExpanded()));
+			if (runIndex != 0) {
+				this.evaluatedStatesAlorithmC.set(expandedStatesListIndex, Integer.valueOf(this.evaluatedStatesAlorithmC.get(expandedStatesListIndex).intValue() + tour.getNumberOfStatesExpanded()));  
+			} else {
+				this.evaluatedStatesAlorithmC.add(Integer.valueOf(tour.getNumberOfStatesExpanded()));
+			}
 
 			TravellingSalesman travellingSalesmanD = new TravellingSalesman(startCity, citiesToTour, TravellingSalesman.LOCAL_LOWER_BOUND_SOLUTION_NEW);
 			tour = travellingSalesmanD.getShortestTour();
-			this.evaluatedStatesAlorithmD.add(Integer.valueOf(tour.getNumberOfStatesExpanded()));
+			if (runIndex != 0) {
+				this.evaluatedStatesAlorithmD.set(expandedStatesListIndex, Integer.valueOf(this.evaluatedStatesAlorithmD.get(expandedStatesListIndex).intValue() + tour.getNumberOfStatesExpanded()));  
+			} else {
+				this.evaluatedStatesAlorithmD.add(Integer.valueOf(tour.getNumberOfStatesExpanded()));
+			}
 
 			TravellingSalesman travellingSalesmanE = new TravellingSalesman(startCity, citiesToTour, TravellingSalesman.INITIAL_GREEDY_SOLUTION_WITH_LB);
 			tour = travellingSalesmanE.getShortestTour();
-			this.evaluatedStatesAlorithmE.add(Integer.valueOf(tour.getNumberOfStatesExpanded()));
+			if (runIndex != 0) {
+				this.evaluatedStatesAlorithmE.set(expandedStatesListIndex, Integer.valueOf(this.evaluatedStatesAlorithmE.get(expandedStatesListIndex).intValue() + tour.getNumberOfStatesExpanded()));  
+			} else {
+				this.evaluatedStatesAlorithmE.add(Integer.valueOf(tour.getNumberOfStatesExpanded()));
+			}
 
 			TravellingSalesman travellingSalesmanF = new TravellingSalesman(startCity, citiesToTour, TravellingSalesman.INITIAL_GREEDY_SOLUTION_WITH_LB_NEW);
 			tour = travellingSalesmanF.getShortestTour();
-			this.evaluatedStatesAlorithmF.add(Integer.valueOf(tour.getNumberOfStatesExpanded()));
+			if (runIndex != 0) {
+				this.evaluatedStatesAlorithmF.set(expandedStatesListIndex, Integer.valueOf(this.evaluatedStatesAlorithmF.get(expandedStatesListIndex).intValue() + tour.getNumberOfStatesExpanded()));  
+			} else {
+				this.evaluatedStatesAlorithmF.add(Integer.valueOf(tour.getNumberOfStatesExpanded()));
+			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -178,17 +205,55 @@ public class TravellingSalesmanClient {
 		return returnValue.toString();
 		
 	}
+	
+	
+	/**
+	 * Compute the average of the number of expanded states for a specific list
+	 */
+	private void averageExpandedStatesList(List<Integer> expandedStatesList) {
+		
+		int listSize = expandedStatesList.size();
+		for (int listIndex = 0; listIndex < listSize; ++listIndex) {
+			
+			expandedStatesList.set(listIndex, Integer.valueOf(expandedStatesList.get(listIndex).intValue() / NUMBER_OF_RUNS_TO_AVERAGE));
+			
+		}
+		
+	}
+	
+	/**
+	 * Compute the average of the number of expanded states
+	 */
+	private void averageTheRunStatistics() {
+		
+		averageExpandedStatesList(this.evaluatedStatesAlorithmA);
+		averageExpandedStatesList(this.evaluatedStatesAlorithmB);
+		averageExpandedStatesList(this.evaluatedStatesAlorithmC);
+		averageExpandedStatesList(this.evaluatedStatesAlorithmD);
+		averageExpandedStatesList(this.evaluatedStatesAlorithmE);
+		averageExpandedStatesList(this.evaluatedStatesAlorithmF);
+		
+	}
 
 	public static void main(String[] args) {
 		
 		TravellingSalesmanClient travellingSalesmanClient = new TravellingSalesmanClient();
-
-		//Run a loop starting from a low count of cities to the maximum
-		for (int cityCounter = MINIMUM_NUMBER_OF_CITIES; cityCounter < MAXIMUM_NUMBER_OF_CITIES; ++cityCounter) {
-			
-			travellingSalesmanClient.runAllTours(cityCounter);
+		int expandedStatesListIndex = 0;
 		
+		//Do multiple runs
+		for (int runIndex = 0; runIndex < NUMBER_OF_RUNS_TO_AVERAGE; ++runIndex) {
+			
+			expandedStatesListIndex = 0;
+			//Run a loop starting from a low count of cities to the maximum
+			for (int cityCounter = MINIMUM_NUMBER_OF_CITIES - 1; cityCounter < MAXIMUM_NUMBER_OF_CITIES; ++cityCounter) {
+				
+				travellingSalesmanClient.runAllTours(cityCounter, runIndex, expandedStatesListIndex++);
+			
+			}
+
 		}
+		
+		travellingSalesmanClient.averageTheRunStatistics();
 		
 		travellingSalesmanClient.printRunStatistics();
 	}
